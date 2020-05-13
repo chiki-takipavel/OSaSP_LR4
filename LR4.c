5d07721c62e62505d86c7bb190901a988c4fc512
Pavel Miskevich <55394253+chiki-takipavel@users.noreply.github.com>
2020-05-13 19:09:40 +0300

    Финальная версия

--- LR4.c
+++ LR4.c
@@ -9,7 +9,6 @@
 #define PARENT "\033[0;31mОсновной:\033[0m"
 #define CHILD1 "\033[0;32mПервый процесс:\033[0m"
 #define CHILD2 "\033[0;33mВторой процесс:\033[0m"
-#define CHILD3 "\033[0;34mТретий процесс:\033[0m"
 #define STRING_SIZE 40
 #define VARIANT 17
 
@@ -17,12 +16,12 @@ int main()
 {
     char *init;
     pid_t child1, child2, child3;
-    child1 = child2 = child3 = 0;
     sigset_t sigset;
+    siginfo_t receive_val;
     char user_string[STRING_SIZE];
     int str_size;
-    char one_char;
     int temp;
+    child1 = child2 = child3 = 0;
 
     // Создаём процессы
     child1 = fork();
@@ -38,49 +37,40 @@ int main()
     if (child1 * child2 * child3 != 0) // Основной процесс
     {
         init = PARENT;
+
+        // Ввод строки
         printf("%s введите строку:\n", init);
         scanf("%s", user_string);
-        printf("%s отправляем в первый процесс %d.\n", init, child1);
 
         sigemptyset(&sigset);
         sigaddset(&sigset, SIGUSR1);
-        sigaddset(&sigset, SIGUSR2);
         sigprocmask(SIG_BLOCK, &sigset, 0);
 
         // Отправляем строку в 1 процесс
+        printf("%s отправляем строку в первый процесс %d.\n", init, child1);
+        union sigval send_str;
         for (int i = 0; i < STRING_SIZE; i++)
         {
-            one_char = user_string[i];
-            for (int j = 0; j < sizeof(char) * 8; j++)
-            {
-                kill(child1, ((one_char >> j) & 0x01) == 0 ? SIGUSR1 : SIGUSR2);
-                sigwait(&sigset, &temp);
-            }
+            send_str.sival_int = user_string[i];
+            sigqueue(child1, SIGUSR1, send_str);
+            sigwait(&sigset, &temp);
         }
         printf("%s строка отправлена.\n", init);
-        kill(child1, SIGUSR1);
 
         // Принимаем размер строки из 1 процесса
-        str_size = 0;
-        for (int j = 0; j < sizeof(int) * 8; j++)
-        {
-            sigwait(&sigset, &temp);
-            temp = ((temp == SIGUSR1) ? 0 : 1);
-            str_size |= (temp << j);
-            kill(child1, SIGUSR1);
-        }
-        sigwait(&sigset, &temp);
+        sigwaitinfo(&sigset, &receive_val);
+        str_size = receive_val.si_value.sival_int;
+        sleep(1);
         printf("%s длина: %d, отправляем во второй процесс.\n", init, str_size);
 
         // Отправляем длину строки во 2 процесс
         setpgid(child2, 0);
-        for (int j = 0; j < sizeof(int) * 8; j++)
+        union sigval trans_size;
+        trans_size.sival_int = str_size;
+        if (!sigqueue(child2, SIGUSR1, trans_size))
         {
-            kill(child2, ((str_size >> j) & 0x01) == 0 ? SIGUSR1 : SIGUSR2);
-            sigwait(&sigset, &temp);
+            printf("%s размер отправлен.\n", init);
         }
-        printf("%s размер отправлен.\n", init);
-        kill(child2, SIGUSR1);
 
         sleep(10);
         printf("%s я проснулся.\n", init);
@@ -91,9 +81,9 @@ int main()
         waitpid(child1, 0, 0);
         waitpid(child2, 0, 0);
 
-        sigfillset(&sigset);
-        sigprocmask(SIG_BLOCK, &sigset, 0);
         kill(child3, SIGINT);
+        system("ps -x");
+        kill(child3, SIGKILL);
         waitpid(child3, 0, 0);
     }
     else
@@ -105,38 +95,24 @@ int main()
 
             sigemptyset(&sigset);
             sigaddset(&sigset, SIGUSR1);
-            sigaddset(&sigset, SIGUSR2);
             sigprocmask(SIG_BLOCK, &sigset, 0);
 
             for (int i = 0; i < STRING_SIZE; i++)
             {
-                one_char = 0;
-                for (int j = 0; j < sizeof(char) * 8; j++)
-                {
-                    if (!sigwait(&sigset, &temp))
-                    {
-                        temp = ((temp == SIGUSR1) ? 0 : 1);
-                        one_char |= (temp << j);
-                        kill(getppid(), SIGUSR1);
-                    }
-                    else
-                    {
-                        j--;
-                    }
-                }
-                user_string[i] = one_char;
+                sigwaitinfo(&sigset, &receive_val);
+                user_string[i] = (char)receive_val.si_value.sival_int;
+                kill(getppid(), SIGUSR1);
             }
             str_size = strnlen(user_string, STRING_SIZE);
-            sigwait(&sigset, &temp);
+            sleep(1);
             printf("%s переданная строка: %s, длина строки %d, отправляем основному процессу.\n", init, user_string, str_size);
 
-            for (int j = 0; j < sizeof(int) * 8; j++)
+            union sigval trans_size;
+            trans_size.sival_int = str_size;
+            if (!sigqueue(getppid(), SIGUSR1, trans_size))
             {
-                kill(getppid(), ((str_size >> j) & 0x01) == 0 ? SIGUSR1 : SIGUSR2);
-                sigwait(&sigset, &temp);
+                printf("%s размер отправлен.\n", init);
             }
-            printf("%s отправлено.\n", init);
-            kill(getppid(), SIGUSR1);
         }
         else if (child2 + child3 == 0) // Второй процесс
         {
@@ -144,19 +120,12 @@ int main()
 
             sigemptyset(&sigset);
             sigaddset(&sigset, SIGUSR1);
-            sigaddset(&sigset, SIGUSR2);
             sigprocmask(SIG_BLOCK, &sigset, 0);
 
             // Получаем длину строки из основного процесса
-            str_size = 0;
-            for (int j = 0; j < sizeof(int) * 8; j++)
-            {
-                sigwait(&sigset, &temp);
-                temp = ((temp == SIGUSR1) ? 0 : 1);
-                str_size |= (temp << j);
-                kill(getppid(), SIGUSR1);
-            }
-            sigwait(&sigset, &temp);
+            sigwaitinfo(&sigset, &receive_val);
+            str_size = receive_val.si_value.sival_int;
+            sleep(1);
             printf("%s длина: %d.\n", init, str_size);
 
             int new_str_size = str_size - VARIANT;
@@ -169,10 +138,9 @@ int main()
                 {
                     if (new_child == 0)
                     {
-                        new_child = fork();
-                        if (new_child == 0)
+                        switch (new_child = fork())
                         {
-                            sleep(1);
+                        case 0:
                             new_child = -1;
                             printf("%s дочерний процесс №%d, мой pid: %d, моя группа: %d.\n", init, i, getpid(), getpgid(0));
                             temp = 0;
@@ -181,11 +149,16 @@ int main()
                                 sleep(1);
                                 printf("%s №%d: процесс жив %d.\n", init, i, temp++);
                             }
-                        }
-                        else
-                        {
+                            break;
+                        case -1:
+                            printf("%s процесс не был создан.\n", init);
+                            i--;
+                            new_child = 0;
+                            break;
+                        default:
                             printf("%s был создан дочерний процесс №%d, его pid: %d, его группа: %d.\n", init, i, getpid(), getpgid(0));
                             new_child = 0;
+                            break;
                         }
                     }
                 }
@@ -197,16 +170,10 @@ int main()
         }
         else // Третий процесс
         {
-            init = CHILD3;
-
             sigemptyset(&sigset);
             sigaddset(&sigset, SIGINT);
-            sigprocmask(SIG_BLOCK, &sigset, 0);
-            if (!sigwait(&sigset, &temp))
-            {
-                system("ps -x");
-                printf("%s я ухожу.\n", init);
-            }
+            signal(SIGINT, SIG_IGN);
+            sigsuspend(&sigset);
         }
     }
     return 0;
